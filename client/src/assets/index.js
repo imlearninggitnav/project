e053fcc0d1f8e6fb87f4fd85f846874ab5fd1f53
Navneet Yadav <navneetyadavkv2@gmail.com>
2024-05-13 11:40:45 +0530

    first commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,10 @@
+import download from './download.png';
+// import logo from './logo.svg';
+import logo from './samplelogo1jpg.jpg';
+import preview from './preview.png';
+
+export {
+  download,
+  logo,
+  preview,
+};
